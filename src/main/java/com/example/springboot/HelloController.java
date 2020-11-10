b04c5d517fcf3fb26848a475d55e736943065cc9
SanjayKumar <sanj@softwareag.com>
2020-11-11 00:13:03 +0530

    This is my 2nd commit in order to change the default username. Date - 11th November 2020.

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,6 +3,10 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *This is a hello controller with Spring Boot application.
+ *
+ */
 @RestController
 public class HelloController {
 
