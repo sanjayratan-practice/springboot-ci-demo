f2bb983e7117701a799d5e85763c54aeec525855
SanjayKumar <sanj@softwareag.com>
2020-11-11 06:25:23 +0530

    Third commmit for testing the workflows on Github. Date - 11 Nov 2020.

--- src/main/java/com/example/springboot/DemoApplication.java
+++ src/main/java/com/example/springboot/DemoApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * This is a spring boot init class.
+ * @author SANJ
+ */
 @SpringBootApplication
 public class DemoApplication {
 
